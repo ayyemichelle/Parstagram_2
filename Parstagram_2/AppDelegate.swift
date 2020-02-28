fc9c90a6ac4d41b34248f0d5a688ab24003aa2de
ayyemichelle <michelle.v.aleman@gmail.com>
2020-02-27 23:21:37 -0800

    parstagram pt 2

--- Parstagram_2/AppDelegate.swift
+++ Parstagram_2/AppDelegate.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window : UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Set applicationId and server based on the values in the Heroku settings.
+              Parse.initialize(
+                  with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                      configuration.applicationId = "Parstagram"
+                      configuration.server = "https://rocky-thicket-78540.herokuapp.com/parse"
+                  })
+              )
+        
+    
         return true
     }
 
---
+++ Parstagram_2/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram_2
+//
+//  Created by Michelle Vasquez-Aleman on 2/26/20.
+//  Copyright © 2020 Michelle Vasquez-Aleman. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
